45a8c814b84c3f3aa10c6acbc92da78753ac56b0
Harisdinus <111202113618@mhs.dinus.ac.id>
2024-05-25 01:13:49 +0700

    nyoba tipsi tipsi

---
+++ src/bin/nyoba.rs
@@ -0,0 +1,35 @@
+use serde_json::{Result, Value};
+
+fn untyped_example() -> Result<()> {
+    // Some JSON input data as a &str. Maybe this comes from the user.
+    let data = r#"
+        {\n
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ],
+            "pepek" : {
+                "hehehe" : "hehehe",
+                "kocok" : {
+                    "kocok1" : "kocokkkk"
+                }
+            }
+        }"#;
+        
+    let data = ToString::to_string(data);
+    let data = data.as_str().trim();
+
+    // Parse the string of data into serde_json::Value.
+    let v: Value = serde_json::from_str(data)?;
+
+    // Access parts of the data by indexing with square brackets.
+    println!("Please call {} at the number {} {}", v["name"], v["phones"][0], v["pepek"]["hehehe"]);
+
+    Ok(())
+}
+
+fn main() {
+    untyped_example().unwrap();
+}
