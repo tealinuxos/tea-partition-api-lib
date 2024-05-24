22e525e94549e935a7fe0e3e5915b715d10a7130
Harisdinus <111202113618@mhs.dinus.ac.id>
2024-05-25 01:14:42 +0700

    uji coba library

--- src/bin/second.rs
+++ src/bin/second.rs
@@ -1,8 +1,13 @@
 use parted_information_extractor_lib::get_partition_information;
 
-
-
-
 fn main() {
-    get_partition_information();
+    // get_partition_information();
+
+    let disk = get_partition_information();
+
+    for i in disk.iter().rev() {
+        println!("=============================================");
+        println!("Jalur disk: {}", i["disk"]["path"]);
+        println!("=============================================");
+    }    
 }
