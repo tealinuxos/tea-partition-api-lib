ac442c6ccdbd8da9250b5a9d39e0b1068d1d2516
gagahsyuja <gghssa1@gmail.com>
2024-06-01 21:17:15 +0700

    Updated test

--- src/bin/test_disk.rs
+++ src/bin/test_disk.rs
@@ -1,12 +1,20 @@
-use parted_information_extractor_lib::disk::*;
+use parted_information_extractor_lib::disk::{ Disk, get_disk_from_json };
 
 fn main()
 {
-    println!("{:#?}", get_disk_from_json::get_disks_info());
-    // println!("{:#?}", get_disk_from_json::get_particular_disk_info("/dev/sda".to_string()));
-    println!("Path: {:#?}", get_disk_path::get_disk_path());
-    // println!("Label /dev/sda: {}", get_disk_label::get_disk_label("/dev/sda".to_string()));
-    // println!("UUID /dev/sda: {}", get_disk_uuid::get_disk_uuid("/dev/sda".to_string()));
-    // println!("Max Partitions /dev/sda: {}", get_disk_max_partition::get_disk_max_partition("/dev/sda".to_string()));
-    // println!("Size /dev/sda: {}", get_disk_size_in_sector::get_disk_size_in_sector("/dev/sda".to_string()));
+    let json = get_disk_from_json::get_all_disk();
+
+    let disks = Disk::from_value_vec(json);
+
+    let mut vec: Vec<String> = Vec::new();
+
+    for i in disks
+    {
+        vec.push(serde_json::to_string_pretty(&i).unwrap());
+    }
+
+    for i in vec
+    {
+        println!("{}", i);
+    }
 }
