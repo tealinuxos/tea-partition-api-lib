21f6d7429952e1ad822e826d16e8b14bd99d4e42
gagahsyuja <gghssa1@gmail.com>
2024-06-01 21:15:19 +0700

    Added test for partition

---
+++ src/bin/test_partition.rs
@@ -0,0 +1,18 @@
+use parted_information_extractor_lib::partition::*;
+
+fn main()
+{
+    let partitions = get_partition_from_json::get_all_partition("/dev/sda".to_string());
+    println!("{:#?}", partitions);
+
+    let partition_vec = Partition::from_value_vec(partitions);
+
+    for i in &partition_vec
+    {
+        let json = serde_json::to_string_pretty(&i).unwrap();
+        
+        println!("{}", json);
+    }
+
+    println!("{}", partition_vec.len());
+}
