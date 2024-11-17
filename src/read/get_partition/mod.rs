6ee0c06b4f38dc1bfb09d9bc194b3368734eb2a5
gagahsyuja <gghssa1@gmail.com>
2024-11-17 22:20:56 +0700

    Using blkid error instead of checking partition table

--- src/read/get_partition/mod.rs
+++ src/read/get_partition/mod.rs
@@ -35,50 +35,53 @@ fn parted_get_list_json_general() -> Vec<Disk>
 
     for i in disks.iter()
     {
-        let blkid = cmd!("blkid", i, "--output", "value").read().expect("Failed to execute blkid");
+        let blkid = cmd!("blkid", i, "--output", "value").read();
 
-        if blkid.contains("gpt") || blkid.contains("mbr")
+        if let Ok(blkid) = blkid
         {
-            let parted = {
+            if blkid.contains("gpt") || blkid.contains("mbr")
+            {
+                let parted = {
 
-                if get_current_uid() != 0
-                {
-                    cmd!("sudo", "parted", "--json", i, "print")
-                }
-                else
-                {
-                    cmd!("parted", "--json", i, "print")
-                }
-            };
+                    if get_current_uid() != 0
+                    {
+                        cmd!("sudo", "parted", "--json", i, "print")
+                    }
+                    else
+                    {
+                        cmd!("parted", "--json", i, "print")
+                    }
+                };
 
-            let parted = parted.read().expect("Failed to run parted");
-            let parted: Value = serde_json::from_str(&parted).expect("Failed to deserialize string into JSON");
-            let parted = parted["disk"].as_object().unwrap();
+                let parted = parted.read().expect("Failed to run parted");
+                let parted: Value = serde_json::from_str(&parted).expect("Failed to deserialize string into JSON");
+                let parted = parted["disk"].as_object().unwrap();
 
-            let disk_path = is_available_string(parted["path"].to_string());
-            let size = is_available_string(parted["size"].to_string());
-            let model = is_available_string(parted["model"].to_string());
-            let transport = is_available_string(parted["transport"].to_string());
-            let label = is_available_string(parted["label"].to_string());
-            let uuid = is_available_string(parted["uuid"].to_string());
-            let max_partition = parted["max-partitions"].to_string().trim().parse().unwrap();
+                let disk_path = is_available_string(parted["path"].to_string());
+                let size = is_available_string(parted["size"].to_string());
+                let model = is_available_string(parted["model"].to_string());
+                let transport = is_available_string(parted["transport"].to_string());
+                let label = is_available_string(parted["label"].to_string());
+                let uuid = is_available_string(parted["uuid"].to_string());
+                let max_partition = parted["max-partitions"].to_string().trim().parse().unwrap();
 
-            let struct_disk = Disk::new(
-                disk_path,
-                size,
-                model,
-                transport,
-                label,
-                uuid,
-                max_partition,
-            );
+                let struct_disk = Disk::new(
+                    disk_path,
+                    size,
+                    model,
+                    transport,
+                    label,
+                    uuid,
+                    max_partition,
+                );
 
-            disk.push(struct_disk);
+                disk.push(struct_disk);
+            }
         }
         else
         {
             let lsblk =
-                cmd!("lsblk", i, "--json", "--paths", "--bytes", "--output", "path,size,model,tran").read().expect("Failed to execute lsblk");
+                cmd!("lsblk", i, "--json", "--paths", "--bytes", "--output", "path,size,model").read().expect("Failed to execute lsblk");
 
             let lsblk: Value = serde_json::from_str(&lsblk).expect("Failed to parse string");
             let lsblk = lsblk["blockdevices"].as_array();
