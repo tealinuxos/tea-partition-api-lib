fe2193fa179d5772206bbb94c659780b56eac320
gagahsyuja <gghssa1@gmail.com>
2025-05-21 22:13:22 +0700

    Fix mountpoint showed as array despite null

--- src/read/get_partition/is_available.rs
+++ src/read/get_partition/is_available.rs
@@ -10,9 +10,29 @@ pub fn is_available_string(input: String) -> Option<String>{
 }
 
 pub fn is_available_vec(input: Option<&Vec<Value>>) -> Option<Vec<Value>>{
-    if input != None {
-        Some(input.unwrap().to_owned())
-    } else {
+    if let Some(input) = input
+    {
+        let mut is_null = false;
+
+        for i in input
+        {
+            if let Value::Null = i
+            {
+                is_null = true;
+            }
+        }
+
+        if is_null
+        {
+            None
+        }
+        else
+        {
+            Some(input.to_owned())
+        }
+    }
+    else
+    {
         None
     }
 }
